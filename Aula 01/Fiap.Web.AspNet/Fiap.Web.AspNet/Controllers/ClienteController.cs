ba3f3bc72c4cd469ccb37427cda78452709c761e
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-08-09 22:16:18 -0300

    Aula 01 criando projeto mvc

---
+++ Aula 01/Fiap.Web.AspNet/Fiap.Web.AspNet/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.AspNet.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View("Index2");
+        }
+
+        public IActionResult Content()
+        {
+            return Content("Teste de Content");
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RedirectToRoute()
+        {
+            return RedirectToRoute( new { controller = "home", action = "index"} );
+        }
+    }
+}
